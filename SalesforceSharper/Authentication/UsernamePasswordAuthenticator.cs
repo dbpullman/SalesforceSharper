cf0c076b05a44ddd057830c0ce148b777f6df450
David Pullman <PullmanD@asme.org>
2019-08-29 12:00:15 -0400

    Added sandbox flag to authenticator

--- SalesforceSharper/Authentication/UsernamePasswordAuthenticator.cs
+++ SalesforceSharper/Authentication/UsernamePasswordAuthenticator.cs
@@ -13,38 +13,46 @@ namespace SalesforceSharper.Authentication
         private readonly string consumerSecret;
         private readonly string username;
         private readonly string password;
+        private readonly bool isSandbox;
         private readonly ISerializer serializer;
 
-        public UsernamePasswordAuthenticator(string consumerKey, string consumerSecret, string username, string password) : this(consumerKey, consumerSecret, username, password, new JsonSerializer())
+        public UsernamePasswordAuthenticator(string consumerKey, string consumerSecret, string username, string password) : this(consumerKey, consumerSecret, username, password, false, new JsonSerializer())
         { }
 
-        public UsernamePasswordAuthenticator(string consumerKey, string consumerSecret, string username, string password, ISerializer serializer)
+        public UsernamePasswordAuthenticator(string consumerKey, string consumerSecret, string username, string password, bool isSandbox) : this(consumerKey, consumerSecret, username, password, isSandbox, new JsonSerializer())
+        { }
+
+        public UsernamePasswordAuthenticator(string consumerKey, string consumerSecret, string username, string password, bool isSandbox, ISerializer serializer)
         {
             this.consumerKey = consumerKey;
             this.consumerSecret = consumerSecret;
             this.username = username;
             this.password = password;
+            this.isSandbox = isSandbox;
             this.serializer = serializer;
         }
 
         public async Task<AuthenticationInfo> Authenticate()
         {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("https://login.salesforce.com/");
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(serializer.ContentType));
-            var content = new FormUrlEncodedContent(new List<KeyValuePair<string, string>>()
+            using (var client = new HttpClient())
             {
-                new KeyValuePair<string, string>("grant_type", "password"),
-                new KeyValuePair<string, string>("client_id", consumerKey),
-                new KeyValuePair<string, string>("client_secret", consumerSecret),
-                new KeyValuePair<string, string>("username", username),
-                new KeyValuePair<string, string>("password", password)
-            });
+                var baseAddress = isSandbox ? "https://login.salesforce.com" : "https://test.salesforce.com";
+                client.BaseAddress = new Uri(baseAddress);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(serializer.ContentType));
+                var content = new FormUrlEncodedContent(new List<KeyValuePair<string, string>>()
+                {
+                    new KeyValuePair<string, string>("grant_type", "password"),
+                    new KeyValuePair<string, string>("client_id", consumerKey),
+                    new KeyValuePair<string, string>("client_secret", consumerSecret),
+                    new KeyValuePair<string, string>("username", username),
+                    new KeyValuePair<string, string>("password", password)
+                });
 
-            var response = await client.PostAsync("services/oauth2/token", content);
-            var responseContent = await response.Content.ReadAsStringAsync();
+                var response = await client.PostAsync("services/oauth2/token", content);
+                var responseContent = await response.Content.ReadAsStringAsync();
 
-            return serializer.Deserialize<AuthenticationInfo>(responseContent);
+                return serializer.Deserialize<AuthenticationInfo>(responseContent);
+            }
 
             //return JsonConvert.DeserializeObject<AuthenticationInfo>(responseContent, new JsonSerializerSettings()
             //{
